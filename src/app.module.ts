fd898268d526c4b879467d36ca512a6b05203e13
Ender13120 <ender.mat@hotmail.de>
2023-04-15 16:32:32 +0200

    missing import appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { BridgeAssistModule } from './bridge-assist/bridge-assist.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProofAssistantService } from './bridge-assist/proof-assistant/proof-assistant.service';
+import { ProofAssistantController } from './bridge-assist/proof-assistant/proof-assistant.controller';
 
 @Module({
   imports: [BridgeAssistModule,ConfigModule.forRoot()],
-  controllers: [AppController],
+  controllers: [AppController, ProofAssistantController],
   providers: [AppService, ProofAssistantService],
 })
 export class AppModule {}
