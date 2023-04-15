fe7113388b4fc9539d01f389ef2b984691188e27
Ender13120 <ender.mat@hotmail.de>
2023-04-15 16:22:58 +0200

    nestjs module abi import issues fix.. and controller routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProofServiceModule } from './proof-service/proof-service.module';
-import { GenerateProofsService } from './generate-proofs/generate-proofs.service';
 import { BridgeAssistModule } from './bridge-assist/bridge-assist.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProofAssistantService } from './bridge-assist/proof-assistant/proof-assistant.service';
 
 @Module({
-  imports: [ProofServiceModule, BridgeAssistModule,ConfigModule.forRoot()],
+  imports: [BridgeAssistModule,ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, GenerateProofsService],
+  providers: [AppService, ProofAssistantService],
 })
 export class AppModule {}
